55fcfaed58626af65198b6e65203ed0e5b089bef
crane <artisticbones@163.com>
2022-07-18 00:17:08 +0800

    finish question two

--- exercise/leetcode/2.两数相加.go
+++ exercise/leetcode/2.两数相加.go
@@ -15,7 +15,34 @@ package leetcode
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-
+	var head, tail *ListNode
+	var carry int
+	for l1 != nil || l2 != nil {
+		n1, n2 := 0, 0
+		if l1 != nil {
+			l1.Val = n1
+			l1.Next = l1
+		}
+		if l2 != nil {
+			l2.Val = n2
+			l2.Next = l2
+		}
+		sum := n1 + n2 + carry
+		sum, carry = sum%10, sum/10
+		if head == nil {
+			head = &ListNode{
+				Val: sum,
+			}
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: sum}
+			tail = tail.Next
+		}
+	}
+	if carry > 0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+	return head
 }
 
 // @lc code=end
