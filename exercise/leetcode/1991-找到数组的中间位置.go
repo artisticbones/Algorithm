5b3c8e228e148aa1e160bb78974b9cd211f8534a
Feitian Cheng <artisticbones@163.com>
2023-03-01 16:08:27 +0800

    update pivotIndex

---
+++ exercise/leetcode/1991-找到数组的中间位置.go
@@ -0,0 +1,30 @@
+package leetcode
+
+/*
+给你一个整数数组nums,请计算数组的中心下标 。
+数组中心下标是数组的一个下标,其左侧所有元素相加的和等于右侧所有元素相加的和。
+如果中心下标位于数组最左端,那么左侧数之和视为0,因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+*/
+
+func pivotIndex(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	left := 0
+	for i := 0; i < len(nums); i++ {
+		if i-1 < 0 {
+			left += 0
+		} else {
+			left += nums[i-1]
+		}
+		if total-nums[i] == 2*left {
+			return i
+		}
+	}
+	return -1
+}
--- exercise/leetcode/646-最长数对链.go
+++ exercise/leetcode/646-最长数对链.go
@@ -10,25 +10,27 @@ import (
 )
 
 // @lc code=start
-// func findLongestChain(pairs [][]int) int {
-// 	sort.Slice(pairs, func(i, j int) bool { return pairs[i][0] < pairs[j][0] })
-// 	dp := make([]int, len(pairs))
-// 	max := func(a, b int) int {
-// 		if b > a {
-// 			return b
-// 		}
-// 		return a
-// 	}
-// 	for i, p := range pairs {
-// 		dp[i] = 1
-// 		for j, q := range pairs[:i] {
-// 			if p[0] > q[1] {
-// 				dp[i] = max(dp[i], dp[j]+1)
-// 			}
-// 		}
-// 	}
-// 	return dp[len(pairs)-1]
-// }
+//
+//	func findLongestChain(pairs [][]int) int {
+//		sort.Slice(pairs, func(i, j int) bool { return pairs[i][0] < pairs[j][0] })
+//		dp := make([]int, len(pairs))
+//		max := func(a, b int) int {
+//			if b > a {
+//				return b
+//			}
+//			return a
+//		}
+//		for i, p := range pairs {
+//			dp[i] = 1
+//			for j, q := range pairs[:i] {
+//				if p[0] > q[1] {
+//					dp[i] = max(dp[i], dp[j]+1)
+//				}
+//			}
+//		}
+//		return dp[len(pairs)-1]
+//	}
+//
 // 贪心算法
 // 要挑选最长数对链的第一个数对时，最优的选择是挑选第二个数字最小的，这样能给挑选后续的数对留下更多的空间。
 // 挑完第一个数对后，要挑第二个数对时，也是按照相同的思路，是在剩下的数对中，第一个数字满足题意的条件下，挑选第二个数字最小的。
