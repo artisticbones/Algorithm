721e4f10c39e4bc805ce3ee713c83fd8dfb4b69e
Feitian Cheng <artisticbones@163.com>
2023-07-25 16:51:04 +0800

    update 206

--- exercise/leetcode/206-反转链表.go
+++ exercise/leetcode/206-反转链表.go
@@ -7,23 +7,38 @@ package leetcode
  *     Next *ListNode
  * }
  */
+//func reverseList(head *ListNode) *ListNode {
+//	if head == nil {
+//		return nil
+//	}
+//	stack, p := make([]int, 0, 1), head
+//
+//	for p != nil {
+//		stack = append(stack, p.Val)
+//		p = p.Next
+//	}
+//
+//	var ret = &ListNode{Val: stack[len(stack)-1], Next: nil}
+//	p = ret
+//	for i := len(stack) - 2; i >= 0; i-- {
+//		q := &ListNode{Val: stack[i], Next: nil}
+//		p.Next = q
+//		p = p.Next
+//	}
+//	return ret
+//}
+
 func reverseList(head *ListNode) *ListNode {
 	if head == nil {
 		return nil
 	}
-	stack, p := make([]int, 0, 1), head
-
-	for p != nil {
-		stack = append(stack, p.Val)
-		p = p.Next
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
 	}
-
-	var ret = &ListNode{Val: stack[len(stack)-1], Next: nil}
-	p = ret
-	for i := len(stack) - 2; i >= 0; i-- {
-		q := &ListNode{Val: stack[i], Next: nil}
-		p.Next = q
-		p = p.Next
-	}
-	return ret
+	return prev
 }
