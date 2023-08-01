02a80b138906e1a50b7ce2f23ef618d4733403bf
Feitian Cheng <artisticbones@163.com>
2023-08-01 11:06:30 +0800

    update 19: smart method

--- exercise/leetcode/19-删除链表的倒数第 N 个结点.go
+++ exercise/leetcode/19-删除链表的倒数第 N 个结点.go
@@ -7,28 +7,44 @@ package leetcode
  *     Next *ListNode
  * }
  */
+//func removeNthFromEnd(head *ListNode, n int) *ListNode {
+//	if head == nil {
+//		return nil
+//	}
+//	dummyHead := &ListNode{Val: 0, Next: nil}
+//
+//	ints := make([]int, 0, 1)
+//	p := head
+//	for p != nil {
+//		ints = append(ints, p.Val)
+//		p = p.Next
+//	}
+//	removeIndex := len(ints) - n
+//	cur := dummyHead
+//	for i, val := range ints {
+//		if i == removeIndex {
+//			continue
+//		}
+//		q := &ListNode{Val: val, Next: nil}
+//		cur.Next = q
+//		cur = cur.Next
+//	}
+//
+//	return dummyHead.Next
+//}
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	if head == nil {
-		return nil
-	}
 	dummyHead := &ListNode{Val: 0, Next: nil}
+	preSlow, slow, fast := dummyHead, head, head
 
-	ints := make([]int, 0, 1)
-	p := head
-	for p != nil {
-		ints = append(ints, p.Val)
-		p = p.Next
-	}
-	removeIndex := len(ints) - n
-	cur := dummyHead
-	for i, val := range ints {
-		if i == removeIndex {
-			continue
+	for fast != nil {
+		if n <= 0 {
+			preSlow = slow
+			slow = slow.Next
 		}
-		q := &ListNode{Val: val, Next: nil}
-		cur.Next = q
-		cur = cur.Next
+		n--
+		fast = fast.Next
 	}
 
+	preSlow.Next = slow.Next
 	return dummyHead.Next
 }
