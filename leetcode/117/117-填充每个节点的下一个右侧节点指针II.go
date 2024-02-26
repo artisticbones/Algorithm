352d5a4e9ae5e30f4da5ab6392bdd5a564a8208c
crane <artisticbones@163.com>
2024-02-26 16:29:57 +0800

    update(117): 二叉树的层序遍历

---
+++ leetcode/117/117-填充每个节点的下一个右侧节点指针II.go
@@ -0,0 +1,40 @@
+package _17
+
+import "container/list"
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	var (
+		queue = list.New()
+	)
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		var (
+			num        = queue.Len()
+			levelNodes = make([]*Node, 0)
+		)
+		for i := 0; i < num; i++ {
+			no := queue.Remove(queue.Front()).(*Node)
+			levelNodes = append(levelNodes, no)
+			if no.Left != nil {
+				queue.PushBack(no.Left)
+			}
+			if no.Right != nil {
+				queue.PushBack(no.Right)
+			}
+		}
+		for i := 0; i < len(levelNodes)-1; i++ {
+			levelNodes[i].Next = levelNodes[i+1]
+		}
+	}
+	return root
+}
